a446962a04baacd25e6283481129f9cf5cfa9bba
yaguine <yagofdez.eb@gmail.com>
2021-07-07 00:18:06 +0200

    Literally everything finished (even javascript) except for main tag part for desktop

--- javascript/script.js
+++ javascript/script.js
@@ -3,17 +3,17 @@ const hamburgerButton = document.getElementById('hamburger-button');
 const hamburgerMenu = document.getElementById('hamburger-menu');
 
 let HAMBURGER_IMG_DIC = {
-    opened:'assets/images/icon-close.svg',
-    closed:'assets/images/icon-hamburger.svg',
+    opened: 'assets/images/icon-close.svg',
+    closed: 'assets/images/icon-hamburger.svg',
 }
 
-hamburgerButton.addEventListener('click', (e) => { 
+hamburgerButton.addEventListener('click', (e) => {
     hamburgerMenu.classList.toggle('hidden');
     let hamburgerImg = hamburgerButton.querySelector('img');
-    if(hamburgerImg.getAttribute('src') === HAMBURGER_IMG_DIC['opened']){
-        hamburgerImg.setAttribute('src',HAMBURGER_IMG_DIC['closed']);
-    } else{
-        hamburgerImg.setAttribute('src',HAMBURGER_IMG_DIC['opened']);
+    if (hamburgerImg.getAttribute('src') === HAMBURGER_IMG_DIC['opened']) {
+        hamburgerImg.setAttribute('src', HAMBURGER_IMG_DIC['closed']);
+    } else {
+        hamburgerImg.setAttribute('src', HAMBURGER_IMG_DIC['opened']);
     }
 });
 
@@ -21,10 +21,21 @@ hamburgerButton.addEventListener('click', (e) => {
 const hamburgerSubMenus = document.querySelectorAll('.hamburger-submenu');
 
 for (let submenu of hamburgerSubMenus) {
-    let img = submenu.querySelector('h4 img');
-    img.addEventListener('click', (e) => {
-        submenu.classList.toggle('hidden');
-        img.classList.toggle('flipped');
-        submenu.querySelector('h4 span').classList.toggle('opened');
-    });
+    let imgList = submenu.querySelectorAll('h4 img');
+    for (let img of imgList) {
+        img.addEventListener('click', (e) => {
+            submenu.classList.toggle('hidden');
+            img.classList.toggle('flipped');
+            submenu.querySelector('h4 span').classList.toggle('opened');
+
+            checkHiddenLarge(submenu)
+        });
+    }
+}
+
+function checkHiddenLarge(submenu){
+    if(submenu.classList.contains('hidden-large')){
+        for(let restSubmenu of hamburgerSubMenus) restSubmenu.classList.add('hidden-large')
+    }
+    submenu.classList.toggle('hidden-large')
 }
